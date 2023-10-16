e6c0f71be04b0e2cd81731da86118de301bac971
xxxroy <xxxluuoyi@gmail.com>
2023-10-16 13:44:07 +0800

    finish-nav-bar

--- src/components/nav-bar.vue
+++ src/components/nav-bar.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="nav-bar">
-    <div class="title">卖座电影</div>
+    <div class="title">电影</div>
     <div class="location" @click="cityClick">{{ cityData.name }} ></div>
+
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue"
-import { router } from 'vue-router';
+import { useRouter } from "vue-router"
 import { showDialog } from "vant"
 import "vant/es/dialog/style"
+const router = useRouter()
+
+//点击左上角跳转到city页面
+const cityClick = () => {
+  console.log("cityClick");
+  router.push({
+    path: "/city",
+  })
+}
 
 const cityData = ref()
 //选择城市相关操作
@@ -24,6 +34,29 @@ if (localStorage.nowCity) {
     router.push({ path: "/city" })
   })
 }
+
+
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.nav-bar {
+  height: 45px;
+  line-height: 45px;
+  color: #fff;
+  background-color: #ff5f16;
+  text-align: center;
+  .title {
+    position: relative;
+    font-size: 18px;
+  }
+  .location {
+    padding: 0 10px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    a {
+      color: #fff;
+    }
+  }
+}
+</style>
--- src/views/movie.vue
+++ src/views/movie.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="movie">
-
-    <div class="nav-bar">
-      <div class="title">卖座电影</div>
-      <div class="location" @click="cityClick">{{ cityData.name }} ></div>
-    </div>
+    <navbar></navbar>
 
     <div class="content">
       <van-tabs
@@ -20,70 +16,31 @@
         </van-tab>
       </van-tabs>
     </div>
+
     <tabbar></tabbar>
   </div>
 </template>
 
 <script setup>
+import navbar from "@/components/nav-bar.vue"
 import movieList from "@/components/movieList.vue"
-import router from "@/router"
+
 import { ref } from "vue"
-import { showDialog } from "vant"
-import "vant/es/dialog/style"
 
-
-const cityData = ref()
-//选择城市相关操作
-if (localStorage.nowCity) {
-  // 通过本地拿到数据
-  cityData.value = JSON.parse(localStorage.nowCity)
-} else {
-  showDialog({
-    message: "请选择城市",
-    theme: "round-button",
-  }).then(() => {
-    router.push({ path: "/city" })
-  })
-}
+const cityId = ref(JSON.parse(localStorage.nowCity))
 
 //定义接口,获取数据(两种拼接方法)
 const hotURL = ref(
-  "getNowPlayingFilmList?cityId=" + cityData.value.cityId + "&pageSize="
+  "getNowPlayingFilmList?cityId=" + cityId.value.cityId + "&pageSize="
 )
 const soonURL = ref(
-  "getComingSoonFilmList?cityId=" + cityData.value.cityId + "&pageSize="
+  "getComingSoonFilmList?cityId=" + cityId.value.cityId + "&pageSize="
 )
 
-//点击左上角跳转到city页面
-const cityClick = () => {
-  router.push({
-    path: "/city",
-  })
-}
 </script>
 
 <style lang="scss" scoped>
-.movie{
+.movie {
   padding-bottom: 50px;
-.nav-bar {
-  height: 45px;
-  line-height: 45px;
-  color: #fff;
-  background-color: #ff5f16;
-  text-align: center;
-  .title {
-    position: relative;
-    font-size: 18px;
-  }
-  .location {
-    padding: 0 10px;
-    position: absolute;
-    top: 0;
-    left: 0;
-    a {
-      color: #fff;
-    }
-  }
-}
 }
 </style>
