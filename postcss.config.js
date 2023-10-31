d404f20e24902fac60c0c2b29752291132d2c0a6
xxxroy <xxxluuoyi@gmail.com>
2023-10-31 11:49:06 +0800

    完轮播图大概

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-// postcss.config.js
-module.exports = {
-  plugins: {
-    'postcss-px-to-viewport': {
-      viewportWidth: 375,
-    },
-  },
-};
--- src/utils/format-date.js
+++
@@ -1,5 +0,0 @@
-import dayjs from "dayjs"
-
-export function formatDate(date){
-  return dayjs.unix(date).format("YYYY.MM.DD")
-}
